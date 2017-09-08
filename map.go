ac61bf4d8c2dc098dc938b9d4f85180f5bcd7163
Casper Szymiczek-Graley <cszymiczek-graley@zendesk>
2017-09-08 11:20:36 +1000

    Playing around with some specific implementations of fold and map

---
+++ map.go
@@ -0,0 +1,38 @@
+package funcs
+
+// Func a function from a to b
+type Func func(int) int
+
+// MapInt : functional map over int array
+func MapInt(f func(int) int, a []int) []int {
+	if f == nil {
+		return nil
+	}
+	var res = make([]int, len(a))
+	for i, x := range a {
+		res[i] = f(x)
+	}
+	return res
+}
+
+// Fold functional fold over int array
+func Fold(init int, f func(int, int) int, a []int) int {
+	var res = init
+	for _, x := range a {
+		res = f(res, x)
+	}
+	return res
+}
+
+// SliceEqual compares slices
+func SliceEqual(a []int, b []int) bool {
+	if len(a) == len(b) {
+		for i := range a {
+			if a[i] != b[i] {
+				return false
+			}
+		}
+		return true
+	}
+	return false
+}
---
+++ map_test.go
@@ -0,0 +1,22 @@
+package funcs
+
+import "testing"
+
+func TestMapInt(t *testing.T) {
+	cases := []struct {
+		inF  Func
+		inA  []int
+		want []int
+	}{
+		{nil, nil, nil},
+		{nil, []int{0, 1, 2}, []int{}},
+		{func(x int) int { return x + 1 }, []int{0, 1, 2}, []int{1, 2, 3}},
+		{func(x int) int { return x * 2 }, []int{0, 1, 2}, []int{0, 2, 4}},
+	}
+	for _, c := range cases {
+		got := MapInt(c.inF, c.inA)
+		if !SliceEqual(got, c.want) {
+			t.Errorf("Map(f, %d) == %d, want %d", c.inA, got, c.want)
+		}
+	}
+}
