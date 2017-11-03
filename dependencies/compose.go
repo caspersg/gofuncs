b61c337c888711c82f012d4c1bb2fa1c887bbb1f
Casper Szymiczek-Graley <cszymiczek-graley@zendesk>
2017-11-03 14:02:34 +1100

    Extract dependencies as functions

--- dependencies/compose.go
+++ dependencies/compose.go
@@ -3,7 +3,7 @@ package dependencies
 // provided api
 type dbclient struct{}
 
-func dbupdate(client dbclient, field string, value interface{}) error {
+func dbupdate(client dbclient, options, field string, value interface{}) error {
 	return nil
 }
 
@@ -20,7 +20,7 @@ func dbconnectionstring(host string) string {
 	return "url:" + host
 }
 
-func BusinessLogicV1(dbhost, query, updatevalue string) (last string, err error) {
+func BusinessLogicV1(dbhost, query, updatevalue, updateOptions string) (last string, err error) {
 	connectiondetails := dbconnectionstring(dbhost)
 	client, err := getdbclient(connectiondetails)
 	if err != nil {
@@ -31,7 +31,49 @@ func BusinessLogicV1(dbhost, query, updatevalue string) (last string, err error)
 		return "", err
 	}
 	for _, result := range results {
-		err = dbupdate(client, result, updatevalue)
+		err = dbupdate(client, updateOptions, result, updatevalue)
+		if err != nil {
+			return "", err
+		}
+		last = result
+	}
+	return last, nil
+}
+
+func BusinessLogicV2(dbhost, query, updatevalue, updateOptions string) (last string, err error) {
+	client, err := makeClient(dbhost)
+	if err != nil {
+		return "", err
+	}
+	return businessLogicV2(
+		func() ([]string, error) {
+			return dbquery(client, query)
+		},
+		func(result string) error {
+			return dbupdate(client, updateOptions, result, updatevalue)
+		},
+	)
+}
+
+func makeClient(dbhost string) (dbclient, error) {
+	connectiondetails := dbconnectionstring(dbhost)
+	client, err := getdbclient(connectiondetails)
+	if err != nil {
+		return dbclient{}, err
+	}
+	return client, nil
+}
+
+func businessLogicV2(
+	query func() ([]string, error),
+	update func(string) error,
+) (last string, err error) {
+	results, err := query()
+	if err != nil {
+		return "", err
+	}
+	for _, result := range results {
+		err = update(result)
 		if err != nil {
 			return "", err
 		}
