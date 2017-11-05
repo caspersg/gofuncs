df131071848fe355039f215db215cde4b16acd3b
Casper Szymiczek-Graley <cszymiczek-graley@zendesk>
2017-11-05 18:39:06 +1100

    Adding tests and refactoring

--- dependencies/compose.go
+++ dependencies/compose.go
@@ -3,9 +3,15 @@ package dependencies
 // Testing, Dependencies and Composition
 // With a lot of dynamic languages, it is easy to replace a given function or method call with a mocked version
 // By changing the behaviour, you make testing existing code much easier.
-// This is very much inspired by https://medium.com/javascript-scene/mocking-is-a-code-smell-944a70c90a6a
 // Another way to make testing easier, especially with static languages is with dependency injection.
+// A different approach is to use function composition which has different trade offs to dependency injection.
+// This is very much inspired by https://medium.com/javascript-scene/mocking-is-a-code-smell-944a70c90a6a
 
+// The assumption I'm making is that there's no active record style library available
+// The way to implement and write tests for the following functionality would be completely
+// different based on the use of that library.
+
+//
 // provided library api
 type dbclient struct{}
 
@@ -19,130 +25,150 @@ func (client dbclient) DbQuery(query string) (result []string, err error) {
 	return []string{}, nil
 }
 
-func newDbClient(config string) (dbclient, error) {
+func NewDbClient(config string) (dbclient, error) {
 	// IO side effect, creating new connection to DB
 	return dbclient{}, nil
 }
 
 // code under source control
-// There's always some amount of processing required to take app config and to use it with a given library.
-func dbconnectionstring(host string) string {
-	return "url:" + host
-}
+
+//
 
 // Basic straight forward code, using db libraries directly.
 // there's no separation of side effects from the logic
 // no dependency is being injected, so nothing can be mocked
 // we basically need a test instance of the DB in order to test this
-// The tests are slower and more prone to fragility,
+// As a result tests are slower and more prone to fragility,
 // so they'll often end up only testing the happy path.
-func BasicVersion(dbhost, query, updatevalue string, updateOptions map[string]string) (last string, err error) {
-	client, err := newDbClient(dbconnectionstring(dbhost))
+func BasicVersion(dbhost, entityID, delta string, updateOptions map[string]string) (lastUpdated string, err error) {
+	connectionString := "url:" + dbhost
+	client, err := NewDbClient(connectionString)
 	if err != nil {
 		return "", err
 	}
+	query := "id=" + entityID
 	results, err := client.DbQuery(query)
 	if err != nil {
 		return "", err
 	}
-	for _, result := range results {
-		err = client.DbUpdate(updateOptions, result, updatevalue)
+	for _, field := range results {
+		err = client.DbUpdate(updateOptions, field, delta)
 		if err != nil {
 			return "", err
 		}
-		last = result
+		lastUpdated = field
 	}
-	return last, nil
+	return lastUpdated, nil
 }
 
-// the first refactoring is usually to extract functions
+//
+
+// One approach to refactoring is to extract functions
 // it may be a bit easier to read,
 // but this has not improved our ease of testing at all
-// If the error handling was more extensive, then that could be partially abstracted in the sub functions
-func ExtractedFunctionVersion(dbhost, query, updatevalue string, updateOptions map[string]string) (last string, err error) {
+// If the error handling was more extensive, then that could also be partially abstracted in the sub functions
+func ExtractedFunctionVersion(dbhost, entityID, delta string, updateOptions map[string]string) (lastUpdated string, err error) {
 	client, err := makeClient(dbhost)
 	if err != nil {
 		return "", err
 	}
-	results, err := client.DbQuery(query)
+	results, err := query(client, entityID)
 	if err != nil {
 		return "", err
 	}
-	return updateAndGetLast(client, results, updatevalue, updateOptions)
-}
-
-func makeClient(dbhost string) (dbclient, error) {
-	return newDbClient(dbconnectionstring(dbhost))
-}
-
-func updateAndGetLast(client dbclient, results []string, updatevalue string, updateOptions map[string]string) (string, error) {
-	last := ""
-	for _, result := range results {
-		err := client.DbUpdate(updateOptions, result, updatevalue)
+	for _, field := range results {
+		err := update(client, field, delta, updateOptions)
 		if err != nil {
 			return "", err
 		}
-		last = result
+		lastUpdated = field
 	}
-	return last, nil
+	return lastUpdated, nil
 }
+
+func dbconnectionstring(host string) string {
+	return "url:" + host
+}
+
+func makeClient(dbhost string) (dbclient, error) {
+	return NewDbClient(dbconnectionstring(dbhost))
+}
+
+func makeQuery(entityID string) string {
+	return "id=" + entityID
+}
+
+func query(client dbclient, entityID string) ([]string, error) {
+	return client.DbQuery(makeQuery(entityID))
+}
+
+func update(client dbclient, field, delta string, updateOptions map[string]string) error {
+	return client.DbUpdate(updateOptions, field, delta)
+}
+
+//
 
 // Basic interface for dependencies being injected.
 // to test this, we'd need to pass in a mock version of the client
-// but this then locks down the implementation to an exact query/update
+// but this then locks down the implementation to a specific query and update implementation,
 // which means we still need an integration or functional test to confirm that it actually works correctly.
-// For example, should the test fail if an additional value in options is passed in?
 type iClient interface {
 	DbUpdate(options map[string]string, field string, value interface{}) error
 	DbQuery(query string) (result []string, err error)
 }
 
-func InterfaceClient(dbhost, query, updatevalue string, updateOptions map[string]string) (last string, err error) {
-	client, err := newDbClient(dbconnectionstring(dbhost))
+func InterfaceClientVersion(dbhost, entityID, delta string, updateOptions map[string]string) (lastUpdated string, err error) {
+	client, err := NewDbClient(dbconnectionstring(dbhost))
 	if err != nil {
 		return "", err
 	}
-	return interfaceClient(client, query, updatevalue, updateOptions)
+	return interfaceClientVersion(client, entityID, delta, updateOptions)
 }
 
-func interfaceClient(client iClient, query, updatevalue string, updateOptions map[string]string) (last string, err error) {
-	results, err := client.DbQuery(query)
+func interfaceClientVersion(client iClient, entityID, delta string, updateOptions map[string]string) (lastUpdated string, err error) {
+	results, err := client.DbQuery(makeQuery(entityID))
 	if err != nil {
 		return "", err
 	}
-	for _, result := range results {
-		err = client.DbUpdate(updateOptions, result, updatevalue)
+	for _, field := range results {
+		err = client.DbUpdate(updateOptions, field, delta)
 		if err != nil {
 			return "", err
 		}
-		last = result
+		lastUpdated = field
 	}
-	return last, nil
+	return lastUpdated, nil
 }
 
 // this function needs to be integration tested to make sure it's doing the right thing
 // a unit test wouldn't be able to confirm that our query or update is actually correct.
-// There are multiple possible queries/updates that would acheive the same result so only an integration test is sufficient.
 // This is because the dependencies being injected (which is the entire contents of this function), produce side effects.
+// There are multiple possible queries/updates that would acheive the same result so only an integration test is sufficient.
+// For example, should the test fail if an additional value in options is passed in?
+
 // If Go had generics, then this composition could be written more generically.
 // but it would still need integration testing, so not a lot is lost
-func FunctionDependencies(dbhost, query, updatevalue string, updateOptions map[string]string) (last string, err error) {
-	return functionDependencies(
+func FunctionDependenciesVersion(dbhost, entityID, delta string, updateOptions map[string]string) (lastUpdated string, err error) {
+	return functionDependenciesVersion(
 		func() (dbclient, error) {
-			return newDbClient(dbconnectionstring(dbhost))
+			return NewDbClient(dbconnectionstring(dbhost))
 		},
-		func(client dbclient) ([]string, error) {
-			return client.DbQuery(query)
-		},
-		updateDep(updatevalue, updateOptions),
+		queryDep(entityID),
+		updateDep(delta, updateOptions),
 	)
+}
+
+func queryDep(entityID string) func(client dbclient) ([]string, error) {
+	return func(client dbclient) ([]string, error) {
+		return client.DbQuery(makeQuery(entityID))
+	}
 }
 
 // If the dependency is more complicated then it can be extracted to a separate function like this
 // This can be tested using mocks and pure functions to confirm it behaves correctly
-func updateDep(updatevalue string, updateOptions map[string]string) func(client dbclient, result string) error {
-	return func(client dbclient, result string) error {
-		return client.DbUpdate(updateOptions, result, updatevalue)
+func updateDep(delta string, updateOptions map[string]string) func(client dbclient, result string) error {
+	return func(client dbclient, field string) error {
+		return client.DbUpdate(updateOptions, field, delta)
 	}
 }
 
@@ -152,11 +178,11 @@ func updateDep(updatevalue string, updateOptions map[string]string) func(client 
 // this can be perfectly unit tested, because the logic can be isolated from the side effects
 // the specific details of the library api are abstracted from the logic as well.
 // I think this function is far more readable now.
-func functionDependencies(
+func functionDependenciesVersion(
 	dbclient func() (dbclient, error),
 	query func(dbclient) ([]string, error),
 	update func(dbclient, string) error,
-) (last string, err error) {
+) (lastUpdated string, err error) {
 	client, err := dbclient()
 	if err != nil {
 		return "", err
@@ -165,12 +191,12 @@ func functionDependencies(
 	if err != nil {
 		return "", err
 	}
-	for _, result := range results {
-		err = update(client, result)
+	for _, field := range results {
+		err = update(client, field)
 		if err != nil {
-			return last, err
+			return lastUpdated, err
 		}
-		last = result
+		lastUpdated = field
 	}
-	return last, nil
+	return lastUpdated, nil
 }
---
+++ dependencies/compose_test.go
@@ -0,0 +1,85 @@
+package dependencies
+
+import (
+	"testing"
+)
+
+var updateOptions = map[string]string{
+	"test": "value",
+}
+
+func TestBasicVersion(t *testing.T) {
+	// TODO setup an actual DB
+	result, err := BasicVersion("test_hostname", "query", "value", updateOptions)
+	if err != nil {
+		t.Errorf("result == %v expected %v", err, nil)
+	}
+	if result != "success" {
+		t.Errorf("result == %v expected %v", result, "success")
+	}
+}
+
+func TestExtractedFunctionVersion(t *testing.T) {
+	// TODO setup an actual DB
+	result, err := ExtractedFunctionVersion("test_hostname", "query", "value", updateOptions)
+	if err != nil {
+		t.Errorf("result == %v expected %v", err, nil)
+	}
+	if result != "success" {
+		t.Errorf("result == %v expected %v", result, "success")
+	}
+}
+
+type clientMock struct{}
+
+func (client clientMock) DbUpdate(options map[string]string, field string, value interface{}) error {
+	// TODO store something to do verifcation
+	return nil
+}
+
+func (client clientMock) DbQuery(query string) (result []string, err error) {
+	// TODO store something to do verifcation
+	return []string{}, nil
+}
+
+func TestInterfaceClientVersion(t *testing.T) {
+	// TODO mock out values, so we get some useful response
+	// probably using a mocking library
+	client := clientMock{}
+	result, err := interfaceClientVersion(client, "query", "value", updateOptions)
+	if err != nil {
+		t.Errorf("result == %v expected %v", err, nil)
+	}
+	if result != "success" {
+		t.Errorf("result == %v expected %v", result, "success")
+	}
+	// TODO verify mock functions were called correctly
+}
+
+func TestFunctionDependencyVersion(t *testing.T) {
+	// very straightforward test setup with no special libaries required
+	// no mocking required, as long as some instance of client can be created
+	expectedClient := dbclient{}
+	expectedUpdates := []string{"a", "b"}
+	client := func() (dbclient, error) { return expectedClient, nil }
+	query := func(client dbclient) ([]string, error) { return expectedUpdates, nil }
+	updateCount := 0
+	update := func(client dbclient, value string) error {
+		if value != expectedUpdates[updateCount] {
+			t.Errorf("result == %v expected %v", value, expectedUpdates[updateCount])
+		}
+		updateCount++
+		return nil
+	}
+	result, err := functionDependenciesVersion(client, query, update)
+	if err != nil {
+		t.Errorf("result == %v expected %v", err, nil)
+	}
+	if updateCount != 2 {
+		t.Errorf("result == %v expected %v", updateCount, 2)
+	}
+	if result != expectedUpdates[1] {
+		t.Errorf("result == %v expected %v", result, expectedUpdates[1])
+	}
+	// TODO verify mock was called correctly
+}
